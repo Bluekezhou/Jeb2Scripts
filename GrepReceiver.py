671052cf451bd89c66326605d85476749ab0a2f0
bluecake <bluekezhou@outlook.com>
2019-11-01 19:09:56 +0800

    update GrepReceiver.py

--- GrepReceiver.py
+++ GrepReceiver.py
@@ -2,35 +2,38 @@ from com.pnfsoftware.jeb.client.api import IScript, IGraphicalClientContext
 from com.pnfsoftware.jeb.core import RuntimeProjectUtil
 from com.pnfsoftware.jeb.core.actions import Actions, ActionContext, ActionXrefsData
 from com.pnfsoftware.jeb.core.units.code import ICodeUnit, ICodeItem
+from Utils import Helper
 
+config_show_exported = False
 
 class GrepReceiver(IScript):
 
-  def run(self, ctx):
-    engctx = ctx.getEnginesContext()
-    if not engctx:
-      print('Back-end engines not initialized')
-      return
+    def run(self, ctx):
+        engctx = ctx.getEnginesContext()
+        if not engctx:
+            print('Back-end engines not initialized')
+            return
 
-    projects = engctx.getProjects()
-    if not projects:
-      print('There is no opened project')
-      return
+        projects = engctx.getProjects()
+        if not projects:
+            print('There is no opened project')
+            return
 
-    prj = projects[0]
-    print('Decompiling code units of %s...' % prj)
+        prj = projects[0]
+        self.helper = Helper(prj)
+        print('Decompiling code units of %s...' % prj)
 
-    self.codeUnit = RuntimeProjectUtil.findUnitsByType(prj, ICodeUnit, False)[0]
-    classes = self.codeUnit.getClasses()
+        self.codeUnit = RuntimeProjectUtil.findUnitsByType(prj, ICodeUnit, False)[0]
+        classes = self.codeUnit.getClasses()
 
-    for cls in classes:
-        if not cls.getName(True).endswith('Receiver'):
-            continue
+        for cls in classes:
+            if not cls.getName(True).endswith('Receiver'):
+                continue
 
-        methods = cls.getMethods()
-        if not methods:
-            continue
-
-        for mtd in methods:
-            if 'onReceive' in mtd.getName(True) and '$' not in  mtd.getAddress():
-                print(mtd.getAddress())
+            if config_show_exported:
+                if not self.helper.isReceiverExported(cls):
+                    continue
+            elif self.helper.isReceiverExported(cls):
+                continue
+            
+            print(cls.getAddress())
---
+++ Utils.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from com.pnfsoftware.jeb.core.units import IXmlUnit
+from com.pnfsoftware.jeb.core import RuntimeProjectUtil
+
+
+class Helper:
+    def __init__(self, project):
+        self.prj = project
+        self.exported_receivers = None
+    
+    def isReceiverExported(self, receiver):
+        if not self.exported_receivers:
+            self.exported_receivers = self.getExportedReceivers()
+
+        return self.getClassPath(receiver) in self.exported_receivers
+
+    def getExportedReceivers(self):
+        return self.getComponentNames('receiver', True)
+
+    def getComponentNames(self, compType, exported):
+        comps = self.getComponent(compType)
+        res = []
+        for comp in comps:
+            if comps[comp]['exported'] == exported:
+                res.append(comp)
+        return res
+
+    def getComponent(self, component):
+        doc = self.getTargetDoc("Manifest")
+        
+        root = doc.getElementsByTagName('manifest').item(0)
+        package = root.getAttribute('package')
+
+        elements = doc.getElementsByTagName(component)
+        components = {}
+        for i in range(elements.getLength()):
+            node = elements.item(i)
+            attr = {
+                'exported': self.toBoolean(node.getAttribute("android:exported"))
+            }
+            
+            name = node.getAttribute("android:name")
+            if name.startswith('.'):
+                name = package + name
+
+            components[name] = attr
+
+        return components
+
+    def toBoolean(self, value):
+        if value == 'false':
+            return False
+        elif value == 'true':
+            return True
+        else:
+            return None
+
+    def getTargetDoc(self, targetXML):
+        units = RuntimeProjectUtil.findUnitsByType(self.prj, IXmlUnit, False)
+        for unit in units:
+            if not unit.isProcessed():
+                unit.process()
+            if unit.getName() == targetXML:
+                doc = unit.getDocument()
+                return doc
+        return None       
+
+    def getClassPath(self, cls):
+        sign = cls.getSignature(True)
+        path = sign[1:-1]
+        path = path.replace('/', '.')
+        return path
+
